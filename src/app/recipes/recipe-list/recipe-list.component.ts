ce8b047d786acdee78bb61798ea5ffe947c83391
GianlucaAneddaHome <gianluca.anedda@libero.it>
2017-10-17 23:29:58 +0200

    work on recipe list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+    new Recipe(
+                'TestRecipe',
+                'test recipe',
+                'http://goodtoknow.media.ipcdigital.co.uk/111/000016ef4/543a_orh412w625/Butter-chicken-recipe.jpg'
+              ),
+    new Recipe(
+                'TestRecipe',
+                'test recipe',
+                'http://goodtoknow.media.ipcdigital.co.uk/111/000016ef4/543a_orh412w625/Butter-chicken-recipe.jpg'
+              )
+   ];
 
   constructor() { }
 
